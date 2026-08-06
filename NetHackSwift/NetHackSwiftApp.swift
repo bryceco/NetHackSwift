c2d122c3b8970f496e744e8042e3f65486c0a656
Bryce Cogswell <bryceco@yahoo.com>
2026-08-06 12:51:49 -0700

    Initial Commit

---
+++ NetHackSwift/NetHackSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetHackSwiftApp.swift
+//  NetHackSwift
+//
+//  Created by Bryce Cogswell on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NetHackSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
